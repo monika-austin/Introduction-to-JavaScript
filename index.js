30b17a937c07c318229398b4d049f8383dd85b85
unknown <ausmonika@gmail.com>
2020-09-09 02:18:17 -0400

    Finished Questions

--- index.js
+++ index.js
@@ -99,17 +99,83 @@ console.log(dogCalculator(15, 1));
 // it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
 // use math.random to determine the computers choice
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number
+function game(playerMove) {
+  const gamePlay = Math.floor(Math.random() * 3) + 1;
+
+  let gameDecision;
+  if (gamePlay == 1) {
+    gameDecision = "rock";
+  } else if (gamePlay == 2) {
+    gameDecision = "paper";
+  } else {
+    gameDecision = "scissors";
+  }
+
+  if (playerMove === "rock") {
+    if (gameDecision === "rock") {
+      return "Draw!" + " against " + gameDecision;
+    } else if (gameDecision === "paper") {
+      return "You lost!" + " against " + gameDecision;
+    } else if (gameDecision === "scissors") {
+      return "You won!" + " against " + gameDecision;
+    }
+  } else if (playerMove === "paper") {
+    if (gameDecision === "rock") {
+      return "You lost!" + " against " + gameDecision;
+    } else if (gameDecision === "paper") {
+      return "Draw" + " against " + gameDecision;
+    } else if (gameDecision === "scissors") {
+      return "You won!" + " against " + gameDecision;
+    }
+  } else if (playerMove === "scissors") {
+    if (gameDecision === "rock") {
+      return "You lost!" + " against " + gameDecision;
+    } else if (gameDecision === "paper") {
+      return "You won!" + " against " + gameDecision;
+    } else if (gameDecision === "scissors") {
+      return "Draw" + " against " + gameDecision;
+    }
+  }
+}
+
+console.log(game("rock"));
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
+function converter(amount) {
+  return amount * 0.62;
+}
+console.log(converter(5));
+
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
+
+function converter(amount) {
+  return amount * 30.48;
+}
+console.log(converter(3));
 
 /************************************************************** Task 6 **************************************************************/
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
+
+function annoyingSong(number) {
+  // i = number
+  // start at number
+  // until number = 0
+  // and we want to countdown so integer minus minus
+
+  let i;
+  for (i = number; i >= 0; i--) {
+    // if we return here it will just return the assigned integer number.
+    // we want to console log each iteration of the loop
+    console.log(i);
+  }
+}
+
+console.log(annoyingSong(15));
 
 /************************************************************** Task 7 **************************************************************/
 //Grade Calculator
@@ -119,6 +185,22 @@ console.log(dogCalculator(15, 1));
 //70s should be Cs
 //60s should be D
 //and anything below 60 should be F
+
+function gradeCalc(percent) {
+  if (percent >= 90) {
+    return console.log("A");
+  } else if (percent >= 80) {
+    return console.log("B");
+  } else if (percent >= 70) {
+    return console.log("C");
+  } else if (percent >= 60) {
+    return console.log("D");
+  } else if (percent <= 60) {
+    return console.log("F");
+  }
+}
+
+console.log(gradeCalc(98));
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
